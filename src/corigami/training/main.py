20abb070684432da9fae9dbff1d0fca146be39fd
javrodriguez <javier.rodriguez.hernaez@gmail.com>
2025-05-23 22:02:47 -0400

    Simplify validation metrics to only show val_loss while maintaining distributed training fixes

--- src/corigami/training/main.py
+++ src/corigami/training/main.py
@@ -188,7 +188,8 @@ class TrainModule(pl.LightningModule):
         outputs = self(inputs)
         criterion = torch.nn.MSELoss()
         loss = criterion(outputs, mat)
-        self.log('val_loss', loss, batch_size=inputs.shape[0], sync_dist=True, on_step=True, on_epoch=True)
+        # Only log the loss, without on_step or on_epoch to keep it simple
+        self.log('val_loss', loss, batch_size=inputs.shape[0], sync_dist=True)
         return loss
 
     def test_step(self, batch, batch_idx):
@@ -196,7 +197,7 @@ class TrainModule(pl.LightningModule):
         outputs = self(inputs)
         criterion = torch.nn.MSELoss()
         loss = criterion(outputs, mat)
-        self.log('test_loss', loss, batch_size=inputs.shape[0], sync_dist=True, on_step=True, on_epoch=True)
+        self.log('test_loss', loss, batch_size=inputs.shape[0], sync_dist=True)
         return loss
 
     def _shared_eval_step(self, batch, batch_idx):
@@ -210,12 +211,12 @@ class TrainModule(pl.LightningModule):
     def on_train_epoch_end(self):
         # Get the training loss from the logged metrics
         train_loss = self.trainer.callback_metrics.get('train_step_loss', torch.tensor(0.0))
-        self.log('train_loss', train_loss, batch_size=self.trainer.train_dataloader.batch_size, sync_dist=True, on_epoch=True)
+        self.log('train_loss', train_loss, batch_size=self.trainer.train_dataloader.batch_size, sync_dist=True)
 
     def on_validation_epoch_end(self):
         # Get the validation loss from the logged metrics
         val_loss = self.trainer.callback_metrics.get('val_loss', torch.tensor(0.0))
-        self.log('val_loss', val_loss, batch_size=self.trainer.val_dataloaders.batch_size, sync_dist=True, on_epoch=True)
+        self.log('val_loss', val_loss, batch_size=self.trainer.val_dataloaders.batch_size, sync_dist=True)
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), 
